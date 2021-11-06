4b34359bcfee16cc3c2b352f65e2444edc1aab0c
alexlfz <alexlopezf365@gmail.com>
2021-11-06 17:27:49 +0100

    Primeras dos pantallas, iniciar sesion y registrarte. Solo diseño

--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.tfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvRegistrate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvRegistrate = (TextView) findViewById(R.id.tvRegistrate);
+        tvRegistrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
