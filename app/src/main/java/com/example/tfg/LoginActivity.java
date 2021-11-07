33977bda52e509b8bf44ca4990d28876152d18d5
alexlfz <alexlopezf365@gmail.com>
2021-11-07 16:32:42 +0100

    Diseño(4)

--- app/src/main/java/com/example/tfg/LoginActivity.java
+++ app/src/main/java/com/example/tfg/LoginActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView tvRegistrate, tvOlvidadoContraseña;
+    Button btnEnviar;
 
 
     @Override
@@ -21,7 +23,17 @@ public class LoginActivity extends AppCompatActivity {
 
         //VARIABLES
         tvRegistrate = (TextView) findViewById(R.id.tvRegistrate);
-        tvOlvidadoContraseña = (TextView) findViewById(R.id.tvOlvidadoContraseña) ;
+        tvOlvidadoContraseña = (TextView) findViewById(R.id.tvOlvidadoContraseña);
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+
+        //ACCEDER A LA APP
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,BrowseActivity.class);
+                startActivity(i);
+            }
+        });
 
 
         //CAMBIAR A ACTIVIDAD DE REGISTRARSE
@@ -29,8 +41,8 @@ public class LoginActivity extends AppCompatActivity {
         tvRegistrate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(i);
+                Intent i2 = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i2);
             }
         });
 
