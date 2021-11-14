bfa6db6961efdaab9436fd31ef8f9032f0520f54
alexlfz <alexlopezf365@gmail.com>
2021-11-14 16:52:44 +0100

    Ultimos cambios antes de la entrega de los diseños

--- app/src/main/java/com/example/tfg/PerfilActivity.java
+++ app/src/main/java/com/example/tfg/PerfilActivity.java
@@ -2,13 +2,32 @@ package com.example.tfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    private Button btnCerrarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        //VARIABLES
+        btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+
+
+        //CERRAR SESION Y NAVEGAR A PANTALLA DE INICIO DE SESIÓN
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PerfilActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
