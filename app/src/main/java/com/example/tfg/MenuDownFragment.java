91719aa508d09ee3d50cb2c7633d3b4f5d1b686d
alexlfz <alexlopezf365@gmail.com>
2021-11-14 15:48:41 +0100

    Añadir mas screens

--- app/src/main/java/com/example/tfg/MenuDownFragment.java
+++ app/src/main/java/com/example/tfg/MenuDownFragment.java
@@ -1,5 +1,6 @@
 package com.example.tfg;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 
 public class MenuDownFragment extends Fragment {
 
+    private Button btnHome, btnFavoritos, btnQuiz, btnPerfil;
 
     public MenuDownFragment() {}
 
@@ -21,6 +25,53 @@ public class MenuDownFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_down, container, false);
+        View v = inflater.inflate(R.layout.fragment_menu_down, container, false);
+
+        btnHome = v.findViewById(R.id.btnHome);
+        btnFavoritos = v.findViewById(R.id.btnFavoritos);
+        btnQuiz = v.findViewById(R.id.btnQuiz);
+        btnPerfil = v.findViewById(R.id.btnPerfil);
+
+        btnHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(getContext(),BrowseActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnFavoritos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i2 = new Intent(getContext(),FavoritosActivity.class);
+                startActivity(i2);
+            }
+        });
+
+        btnQuiz.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i2 = new Intent(getContext(),FavoritosActivity.class);
+                startActivity(i2);
+            }
+        });
+
+        btnFavoritos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i3 = new Intent(getContext(),QuizActivity.class);
+                startActivity(i3);
+            }
+        });
+
+        btnPerfil.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i4 = new Intent(getContext(),PerfilActivity.class);
+                startActivity(i4);
+            }
+        });
+
+        return v;
     }
 }
